350478211191a05445c5dd267435f8a09226867b
Zhuoyang ye <737475845@qq.com>
2024-07-29 14:24:15 -0700

    Print stabilizer and destabilizer

--- tableau.py
+++ tableau.py
@@ -3,12 +3,11 @@ import numpy as np
 
 
 
-
 class tableau:
     
     
-    def __init__(num__qubit,self) -> None:
-        self._num_qubit = num__qubit
+    def __init__(self,num_qubit) -> None:
+        self._num_qubit = num_qubit
         #Store instructions in tuples
         self._instructions=[]
         
@@ -19,8 +18,7 @@ class tableau:
         
         
     def init_tableau(self):
-        self._tableau = np.zeros((2*self._num_qubit, 2*self._num_qubit+1), dtype=int)
-        self._tableau[0:self._num_qubit, 0] = 1        
+        self._tableau = np.zeros((2*self._num_qubit, 2*self._num_qubit+1), dtype=int)   
         diagonal_indices=np.arange(2*self._num_qubit)
         self._tableau[diagonal_indices, diagonal_indices] = 1
         
@@ -30,12 +28,44 @@ class tableau:
     For example:
     ["XXI","XYI",...]
     '''    
-    def get_stabilizers():   
-        pass
+    def get_stabilizers(self):   
+        stabilizer_list=[]
+        for i in range(0,self._num_qubit):
+            
+            stabilizer=""
+            for j in range(0,self._num_qubit):
+                if self._tableau[i,j]==1 and self._tableau[i,j+self._num_qubit]==1:
+                    stabilizer+="Y"
+                elif self._tableau[i,j]==1 and self._tableau[i,j+self._num_qubit]==0:
+                    stabilizer+="Z"
+                elif self._tableau[i,j]==0 and self._tableau[i,j+self._num_qubit]==1:
+                    stabilizer+="X"                   
+                else:
+                    stabilizer+="I"
+            if self._tableau[i,2*self._num_qubit]==1:
+                stabilizer="-"+stabilizer
+            stabilizer_list.append(stabilizer)
+        return stabilizer_list
     
     
-    def get_destabilizer():
-        pass
+    def get_destabilizer(self):
+        destabilizer_list=[]
+        for i in range(self._num_qubit,self._num_qubit*2):
+            
+            destabilizer=""
+            for j in range(0,self._num_qubit):
+                if self._tableau[i,j]==1 and self._tableau[i,j+self._num_qubit]==1:
+                    destabilizer+="Y"
+                elif self._tableau[i,j]==1 and self._tableau[i,j+self._num_qubit]==0:
+                    destabilizer+="Z"
+                elif self._tableau[i,j]==0 and self._tableau[i,j+self._num_qubit]==1:
+                    destabilizer+="X"                   
+                else:
+                    destabilizer+="I"
+            if self._tableau[i,2*self._num_qubit]==1:
+                destabilizer="-"+destabilizer
+            destabilizer_list.append(destabilizer)
+        return destabilizer_list
     
     
     def CNOT(self, control, target):
