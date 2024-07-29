f3cb7c557d602f01e9fcb545fd837b16827ca839
Zhuoyang ye <737475845@qq.com>
2024-07-29 13:59:38 -0700

    First commit

---
+++ tableau.py
@@ -0,0 +1,44 @@
+import numpy as np
+
+
+
+
+
+class tableau:
+    
+    
+    def __init__(num__qubit,self) -> None:
+        self._num_qubit = num__qubit
+        
+        
+    def init_tableau(self):
+        self._tableau = np.zeros((2*self._num_qubit, 2*self._num_qubit+1), dtype=int)
+        self._tableau[0:self._num_qubit, 0] = 1        
+        diagonal_indices=np.arange(2*self._num_qubit)
+        self._tableau[diagonal_indices, diagonal_indices] = 1
+        
+    '''
+    Return the list of stabilizers in the string format
+    For example:
+    ["XXI","XYI",...]
+    '''    
+    def get_stabilizers():   
+        pass
+    
+    
+    def get_destabilizer():
+        pass
+    
+    
+    def CNOT(self, control, target):
+        pass
+    
+    def Hadamard(self, target):
+        pass
+    
+    def Phase(self, target):
+        pass
+    
+    def measure(self, target):
+        pass
+    
